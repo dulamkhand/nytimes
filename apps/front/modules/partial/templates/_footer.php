f8618b036d89a7c304811487a981359065ee54ff
dulamkhand <handaa.1224@gmail.com>
2015-07-24 22:53:03 +0900

    handaa

--- apps/front/modules/partial/templates/_footer.php
+++ apps/front/modules/partial/templates/_footer.php
@@ -4,26 +4,32 @@
 		<?php if($rs):?>
 				<?php include_partial("partial/banner", array('rs'=>$rs, 'width'=>1000, 'height'=>150));?>
 		<?php endif?>
-		<div style="text-transform:uppercase;font-size:14px;text-align:center;margin:0 0 5px 0;">
-				Та сайтад тавигдсан мэдээллийг ашигласан тохиолдолд сайтын нэрийг заавал дурдана уу. <?php echo sfConfig::get('app_domain_www')?>
-		</div>
 </div>
 
 <?php include_partial("partial/clients", array());?>
 
 <div id="footer">
-	<div class="wrapper">
-    <div class="left" style="margin:0 10px 0 0;">
-				<!--<a><img src="http://hitwebcounter.com/counter/counter.php?page=5945219&style=0038&nbdigits=5&type=page&initCount=50000" title="" alt="" border="0"></a>-->
-				<?php //echo hits_webcounter(0);?>
-		</div>
-    <div class="right" style="margin:0 10px 0 0;">
-        <ul>
-            <li class="left"><a href="<?php echo url_for('main/about')?>">Вэбийн тухай</a> &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</li>
-            <li class="left"><a href="<?php echo url_for('main/advertisement')?>">Реклам байршуулах</a> &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</li>
-            <li class="left"><a href="<?php echo url_for('main/contact')?>">Холбогдох</a></li>
-        </ul>
-    </div>
-    <br clear="all">
-  </div>
-</div>
+	<div class="wrapper" style="border-top:2px solid #e2e2e2;padding:2px 0px;">
+		<div style="border-top:1px solid #e2e2e2;border-bottom:1px solid #e2e2e2;padding:5px 0px;">
+				<!--logo-->
+        <a href="<?php echo url_for('@homepage')?>" style="">
+            <?php echo image_tag('logo377.png', array('style'=>'max-width:200px;'))?>
+        </a>
+        <br clear="all"> 	
+        <br clear="all">
+		
+		    <div class="left" style="margin:0 10px 0 0;">
+						<!--<a><img src="http://hitwebcounter.com/counter/counter.php?page=5945219&style=0038&nbdigits=5&type=page&initCount=50000" title="" alt="" border="0"></a>-->
+						<?php //echo hits_webcounter(0);?>
+				</div>
+		    <div class="right" style="margin:0 10px 0 0;">
+		        <ul>
+		            <li class="left"><a href="<?php echo url_for('main/about')?>">Вэбийн тухай</a> &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</li>
+		            <li class="left"><a href="<?php echo url_for('main/advertisement')?>">Реклам байршуулах</a> &nbsp;&nbsp;|&nbsp;&nbsp;&nbsp;</li>
+		            <li class="left"><a href="<?php echo url_for('main/contact')?>">Холбогдох</a></li>
+		        </ul>
+		    </div>
+  	</div>
+  	<span style="color:#000;font-family:Arial;font-size:11px;">&copy;2015 <?php echo sfConfig::get('app_domain_www')?></span>
+  </div><!--wrapper-->
+</div><!--footer-->
